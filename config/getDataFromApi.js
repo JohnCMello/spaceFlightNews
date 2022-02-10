a0a3e2eb23704add20532d58fb24e2f187296d15
JohnCMello <johncharlesmello@gmail.com>
2022-02-10 16:00:40 -0300

    get articles count

--- config/getDataFromApi.js
+++ config/getDataFromApi.js
@@ -2,11 +2,17 @@ const axios = require('axios');
 
 const ArticleModel = require('../src/Models/ArticleModel')
 
+const getArticlesCount = async () => {
+  const { data } = await axios('https://api.spaceflightnewsapi.net/v3/articles/count')
+  return data
+}
+
 const getDataFromApi = async () => {
   try {
-    const { data } = await axios(`https://api.spaceflightnewsapi.net/v3/articles`)
+    const articlesCount = await getArticlesCount()
+    const apiUrl = `https://api.spaceflightnewsapi.net/v3/articles?_limit=${articlesCount}`
+    const { data } = await axios(apiUrl)
     await ArticleModel.insertMany(data)
-    console.log(data)
   } catch (error) {
     console.log(error)
   }
