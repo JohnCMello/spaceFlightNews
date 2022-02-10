ec783775385f500ab0350612411777faf1d97c1c
JohnCMello <johncharlesmello@gmail.com>
2022-02-10 16:01:24 -0300

    add article controller

---
+++ src/Controllers/ArticleController.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
+
+const ArticleModel = require('../Models/ArticleModel');
+
+// @Controllers=================================================================
+
+//GET
+const getArticles = () => ArticleModel.find({});
+const getArticlesById = (id) => ArticleModel.findOne({ _id: ObjectId(id) });
+
+//POST
+const postArticle = (body) => ArticleModel.create(body);
+
+//PUT
+const updateArticle = (id, updates) => ArticleModel.findByIdAndUpdate({ _id: ObjectId(id) }, updates, { new: true }
+)
+
+//DELETE
+const deleteArticle = (id) => ArticleModel.findByIdAndRemove({ _id: ObjectId(id) });
+
+module.exports = {
+  getArticles,
+  getArticlesById,
+  postArticle,
+  updateArticle,
+  deleteArticle
+}
