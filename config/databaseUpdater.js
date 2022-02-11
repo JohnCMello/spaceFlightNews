ab8f0387585bd801545db6205750cd51a4dd323e
JohnCMello <johncharlesmello@gmail.com>
2022-02-11 16:35:07 -0300

    remove verifications that were breaking the script

--- config/databaseUpdater.js
+++ config/databaseUpdater.js
@@ -1,50 +1,50 @@
 const axios = require('axios');
 
-const ArticleModel = require('../src/Models/ArticleModel')
+const ArticleModel = require('../src/Models/ArticleModel');
 
 // TODO: Handle Errors
 
 const getHighestId = async () => {
   try {
-    const idList = await ArticleModel.distinct('id') || [1]
-    const highestId = idList.sort((a, b) => b - a)
-    return highestId[0]
+    const idList = await ArticleModel.distinct('id') || [1];
+    const highestId = idList.sort((a, b) => b - a);
+    return highestId[0];
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 const getArticlesCount = async () => {
-  const { data } = await axios('https://api.spaceflightnewsapi.net/v3/articles/count')
-  return data
-}
+  const { data } = await axios('https://api.spaceflightnewsapi.net/v3/articles/count');
+  return data;
+};
 
 const insertNewArticlesIntoDB = async () => {
   try {
-    const highestId = await getHighestId()
-    const apiUrl = `https://api.spaceflightnewsapi.net/v3/articles?id_gt=${highestId}`
-    const { data } = await axios(apiUrl)
-    console.log(highestId)
-    return await ArticleModel.insertMany(data)
+    const highestId = await getHighestId();
+    const apiUrl = `https://api.spaceflightnewsapi.net/v3/articles?id_gt=${highestId}`;
+    const { data } = await axios(apiUrl);
+    console.log(highestId);
+    return await ArticleModel.insertMany(data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 const getDataFromApi = async () => {
   try {
-    const articleList = await ArticleModel.find({})
-    const articlesCount = await getArticlesCount()
-    const apiUrl = `https://api.spaceflightnewsapi.net/v3/articles?_limit=${articlesCount}`
-    const { data } = await axios(apiUrl)
-    if (articleList.length === 0) return await ArticleModel.insertMany(data)
-    return
+    const articleList = await ArticleModel.find({});
+    const articlesCount = await getArticlesCount();
+    const apiUrl = `https://api.spaceflightnewsapi.net/v3/articles?_limit=${articlesCount}`;
+    const { data } = await axios(apiUrl);
+    if (articleList.length === 0) return await ArticleModel.insertMany(data);
+    return;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 module.exports = {
   getDataFromApi,
   insertNewArticlesIntoDB
-} 
+};
