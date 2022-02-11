1b96842bbc3868675325eb304adf98a7a6b059e7
JohnCMello <johncharlesmello@gmail.com>
2022-02-11 16:35:43 -0300

    add cron insertNewArticles @9am every day

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 require('dotenv').config({ path: './config/config.env' });
 const connectDB = require('./config/db');
-const cron = require('node-cron')
+const cron = require('node-cron');
 const express = require('express');
-const app = express()
+const app = express();
 
 // TODO: Handle Errors
 
 const {
   getDataFromApi,
   insertNewArticlesIntoDB
-} = require('./config/databaseUpdater')
+} = require('./config/databaseUpdater');
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/', require('./routes/index'))
-app.use('/articles', require('./routes/articles'))
+app.use('/', require('./routes/index'));
+app.use('/articles', require('./routes/articles'));
 
 const PORT = process.env.PORT || 3000;
 
@@ -23,9 +23,9 @@ const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
       console.log(`Server running on port: ${PORT}`);
     })
-    connectDB()
-    getDataFromApi()
-    cron.schedule('0 9 * * *', () => insertNewArticlesIntoDB())
+    connectDB();
+    getDataFromApi();
+    cron.schedule('0 9 * * *', () => insertNewArticlesIntoDB());
   } catch (error) {
     console.log(error);
   }
